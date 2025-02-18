d82a361b55744781a3d0415cec54191111675783
Sk4is-1 <adrianperezagredano@gmail.com>
2025-02-18 10:33:22 +0100

    js added

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const pauseBtn = document.getElementById("pauseBtn");
 const scoreElement = document.getElementById("score");
 const highscoreElement = document.getElementById("highscore");
 
+const ROWS = 20, COLS = 10, SIZE = 30;
+let board = Array.from({ lenght: ROWS }, () => Array(COLS).fill(0));
+
+let isPaused = false;
+let gameInterval;
+let score = 0;
+let highscore = localStorage.getItem("highScore") || 0;
+highscoreElement.textContent = highscore;
