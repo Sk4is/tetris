2dc976a397c934cc3b1b205f6ba39f9c60f5c7b1
Sk4is-1 <adrianperezagredano@gmail.com>
2025-02-18 11:12:36 +0100

    js added

--- main.js
+++ main.js
@@ -12,3 +12,31 @@ let gameInterval;
 let score = 0;
 let highscore = localStorage.getItem("highScore") || 0;
 highscoreElement.textContent = highscore;
+
+const SHAPES = [
+    [[1, 1, 1, 1]], // línea
+    [[1, 1], [1, 1]], // Cuadrado
+    [[0, 1, 0], [1, 1, 1]], // "T" inversa
+    [[1, 1, 0], [0, 1, 1]], // "Z"
+    [[0, 1, 1], [1, 1, 0]], // "S"
+];
+
+let currentPiece = {
+    shape: SHAPES[Math.floor(Math.random() * SHAPES.length)],
+    x: 3,
+    y: 0
+};
+
+function drawBoard() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    board.forEach((row, y) => row.forEach((value, x) => {
+        if (value) drawBlock(x, y, "cyan");
+    }));
+}
+
+function drawBlock(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * SIZE, y * SIZE, SIZE, SIZE);
+    ctx.strokeStyle = "black";
+    ctx.strokeReact(x * SIZE, y * SIZE, SIZE, SIZE);
+} 
