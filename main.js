073d3f8006544fc8e3c0741eab734b536308e258
Sk4is-1 <adrianperezagredano@gmail.com>
2025-02-18 09:58:34 +0100

    html and css done

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const
+const canvas = document.getElementById("tetris");
+const ctx = canvas.getContext("2d");
+const pauseBtn = document.getElementById("pauseBtn");
+const scoreElement = document.getElementById("score");
+const highscoreElement = document.getElementById("highscore");
+
